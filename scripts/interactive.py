b037868c151e29eb7bf18295cd5e92f1b27e6b10
phstoot <stoot.philip@gmail.com>
2026-05-24 12:46:17 +0200

    init positions

--- scripts/interactive.py
+++ scripts/interactive.py
@@ -2,5 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from cosmosim.simulation import Universe
 
-test = Universe(10, 10, 10, 1, 1, 0.5)
+test = Universe()
 print(test)
+
+
+n_cells = 10
+n_particles = 5
+
+positions = []
+fill_axis = np.linspace(0, n_cells, n_particles, endpoint=False, dtype=float)
+
+for i in range(n_particles):
+    for j in range(n_particles):
+        for k in range(n_particles):
+            x = fill_axis[i]
+            y = fill_axis[j]
+            z = fill_axis[k]
+            positions.append([x, y, z])
+
+positions = np.array(positions)
--- src/cosmosim/simulation.py
+++ src/cosmosim/simulation.py
@@ -27,26 +27,28 @@ import pandas as pd
 class Universe:
     def __init__(
             self,
-            size: float,
-            n_particles: int,
-            n_grid: int, # rename to n_cells?
-            delta_t: float,
-            redshift: float,
-            scale_factor: float,
+            n_particles: int = 32,
+            n_cells: int = 64, 
+            mass: float = 1,
+            delta_t: float = 0.1, # decide later, we also need to change to use scale factor as timestep
+            redshift: float = 1,
+            scale_factor: float = 0.5,
             time_period: tuple = (0, 10),
     ):
-        self.size = size
-        self.n_particles = n_particles
-        self.n_grid = n_grid
+        
+        self.n_particles = n_particles # we can use n_cells as equivalent to 'size' in the first assignment, since the size of a cell is 1
+        self.n_cells = n_cells
+        self.mass = mass
         self.delta_t = delta_t
         self.time_period = time_period
         self.redshift = redshift
         self.scale_factor = scale_factor
 
         # initialize
-        self.positions = 0
-        self.momenta = 0
-        self.potential = 0
+        self.potential = np.zeros((self.n_cells, self.n_cells, self.n_cells))
+        self.density = np.zeros((self.n_cells, self.n_cells, self.n_cells))
+        self.positions = self._init_positions()
+        self.momenta = np.zeros((self.n_particles, self.n_particles, self.n_particles))
         self.time_current = 0
         self._status = 0 # let's work with numerical codes: 0 = initialized, 1 = running and 2 = complete or smth like that
         
@@ -63,8 +65,8 @@ class Universe:
 
     def __repr__(self):
         return (
-            f"Universe(size={self.size}, n_particles={self.n_particles}, "
-            f"n_grid={self.n_grid}, redshift={self.redshift}, scale_factor={self.scale_factor},"
+            f"Universe(n_particles={self.n_particles}, "
+            f"n_cells={self.n_cells}, redshift={self.redshift}, scale_factor={self.scale_factor},"
             f"time_current={self.time_current}, _status={self._status})"
             )
     
@@ -80,17 +82,42 @@ class Universe:
             f"Redshift: z = {self.redshift:.4f}\n"
             f"Scale factor: a = {self.scale_factor:.4f}\n\n"
             f"Parameters:\n"
-            f"  Box size: {self.size}\n"
-            f"  Particles: {self.n_particles}\n"
-            f"  Grid cells: {self.n_grid}³\n"
+            f"  Particles: {self.n_particles}³\n"
+            f"  Grid cells: {self.n_cells}³\n"
             f"  Timestep: {self.delta_t}\n"
         )
 
-    def init_grid(self):
-        pass
+    def _init_positions(self):
+        # For a realistic simulation, we need to either write a sophisticated code to generate IC's, or use other code for this. 
+        # For now, start with regularly placed particles throughout the volume and give them small random deviations
+        positions = []
+        fill_axis = np.linspace(0, self.n_cells, self.n_particles, endpoint=False, dtype=float)
 
-    def density_field(self):
-        """Method to interpolate the density field from the current state of the simulation
+        for i in range(self.n_particles):
+            for j in range(self.n_particles):
+                for k in range(self.n_particles):
+                    x = fill_axis[i]
+                    y = fill_axis[j]
+                    z = fill_axis[k]
+                    positions.append([x, y, z])
+        positions = np.array(positions)
+        
+        # perturb each particle slightly, random for now
+        rng = np.random.default_rng()
+        for p in positions:
+            p += rng.random(3)
+        return positions
+
+    def density_npg(self):
+        """Method to interpolate the mesh density field from the current particle positions, using the Nearest Grid Point (NPG) method.
+        This simply means giving the parent grid cell the mass of the particle.
+        """
+        for i, p in enumerate(self.positions):
+           cell_index = p.astype(int)
+           self.density[tuple(cell_index)] += self.mass
+
+    def density_cic(self):
+        """Method to interpolate the mesh density field from the current particle positions, using the Cloud In Cell (CIC) method.
         """
         pass
 
