d974fd6e78bd5abc1304b3cd0da7306db602c98b
NilsThiessen <Nils.Thiessen@web.de>
2026-06-12 00:21:59 +0200

    Some commits

---
+++ scripts/example_LambdaCDM.py
@@ -0,0 +1,60 @@
+###############################################
+# EXAMPLE RUN: Show basic application of simulatio:
+# This script runs a random simulation, 
+# visualizes the analysis methods, 
+# follows the same script with ΛCDM initial conditions.
+#
+# outputs: 
+#  - various 3D and sliced plots
+#  - various 3D and sliced animations
+#  - Power spectrum plots
+###############################################
+
+import itertools
+from time import time, sleep
+from pathlib import Path
+from matplotlib import scale
+import numpy as np
+import matplotlib.pyplot as plt
+from time import sleep
+from tqdm import tqdm
+from PIL import Image
+import pandas as pd
+from scipy import fft
+from cosmosim.utils import *
+
+
+
+## Initialize normal simulation (random)
+section("Overview")
+print("This script demonstrates a random simulation.")
+spacer()
+sleep(1)
+
+print("Comme")
+
+
+print()
+
+
+# Run for 900 steps
+
+
+# Visualize evolution, Final state (as plot) and Power Spectrum
+
+
+
+## Initialize \lambda CDM simulation
+
+
+# Same steps as before
+
+
+
+# Some comment on this (what changed? Filaments observed? Requires bigger model)
+
+
+
+
+
+# Potentially bigger simulation
